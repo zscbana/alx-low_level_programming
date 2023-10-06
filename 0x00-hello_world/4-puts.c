c45d7abbe9cf8997dbbe26d3f6f12cc22ba5d8da
zscbana <zscbana@gmail.com>
2023-10-06 04:50:15 +0300

    auto commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+/*
+* main - Entry point
+*
+* Description: A C program that prints with put function
+*
+* Return: Alawys 0 (Success)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
