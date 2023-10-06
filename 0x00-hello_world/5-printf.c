a5557363242672f6a8f6e07650f2993a46602a64
zscbana <zscbana@gmail.com>
2023-10-06 05:02:53 +0300

    auto commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: A C program usin printf 
+ *
+ * return always 0 (true)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
