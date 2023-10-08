38ba1a1b7f38115aaebd64f695e87d14f20ee93e
zscbana <zscbana@gmail.com>
2023-10-08 21:57:19 +0300

    auto commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print 0-9 using putchar
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0')
+			num++;
+	}
+	putchar('\n);
+	return (0);
+}
