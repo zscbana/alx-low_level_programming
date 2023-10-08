d42ca92e81c23d00cfb184543ed0e4fe653b69e2
zscbana <zscbana@gmail.com>
2023-10-08 20:16:13 +0300

    auto commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all aplhabet
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
