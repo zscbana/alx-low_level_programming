03cb9cbdbd24d8e77cf57184c3a635643e2783de
zscbana <zscbana@gmail.com>
2023-10-08 21:45:22 +0300

    auto commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints from 0-9
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
