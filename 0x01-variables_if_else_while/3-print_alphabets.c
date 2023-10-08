3a2725451f4e6018d9bd4c66cbaec1258d95468a
zscbana <zscbana@gmail.com>
2023-10-08 20:32:16 +0300

    auto commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all uppercase and lowercase from a-z and A-Z
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/* print a-z */
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	/* print A-Z */
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
