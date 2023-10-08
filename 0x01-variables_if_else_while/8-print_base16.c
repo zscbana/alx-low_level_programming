a9f971908dab1f7359774094b5eb5a7b9086eee7
zscbana <zscbana@gmail.com>
2023-10-08 22:27:39 +0300

    auto commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all number of base 16
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 48;
+
+	while (num <= 102)
+	{
+		putchar(num);
+		if (ch == 57)
+			ch += 39;
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
