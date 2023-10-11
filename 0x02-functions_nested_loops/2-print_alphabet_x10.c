4ae525860facac5c9a528a5ad3ccf5ce72233310
zscbana <zscbana@gmail.com>
2023-10-11 03:47:40 +0300

    auto commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print from a to z 10 times
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int c, i = 0;
+
+	while (i < 10)
+	{
+		for (int c = 'a'; c <= 'z'; ++c)
+		{
+
+			_putchar(c);
+		}
+		i++;
+
+		_putchar('\n');
+	}
+}
