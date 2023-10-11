220344d45513f706bf25006e8f56e8bfcfb6e100
zscbana <zscbana@gmail.com>
2023-10-11 05:40:00 +0300

    auto commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - provide the absoulte of number
+ *@i: number
+ *
+ * Return: the absulte value of i
+*/
+
+int _abs(int i)
+{
+	int abs(i);
+	return (i);
+}
