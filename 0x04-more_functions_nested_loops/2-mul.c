01b202808916c76ffc24074bd6bff35b5f337aa9
zscbana <zscbana@gmail.com>
2023-10-12 19:42:25 +0300

    auto commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function used to multiple two numbers
+ * @a: first number
+ * @b: secound number
+ *
+ * Return: return results
+ */
+
+int mul(int a, int b)
+{
+	int a, b, num;
+		num = a * b;
+	return (num);
+}
