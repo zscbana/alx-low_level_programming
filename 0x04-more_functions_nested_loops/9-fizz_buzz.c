e965235684f24038db913b3c8fbffab6e9e7ac5f
zscbana <zscbana@gmail.com>
2023-10-13 00:53:13 +0300

    auto commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,11 +16,12 @@ int main(void)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0 && !(i % 5 == 0))
 			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
-		if (i < 100)
+
+		if (i != 100)
 			printf(" ");
 		else
 			printf("\n");
