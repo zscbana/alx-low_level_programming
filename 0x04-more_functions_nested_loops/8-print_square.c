01aa6871739fd1afa6c9896d64e144f4dcdf2758
zscbana <zscbana@gmail.com>
2023-10-13 00:29:35 +0300

    auto commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@ void print_square(int size)
 {
 	int row, column;
 
-	for (row = 1; row < size; row++)
+	for (row = 1; row <= size; row++)
 	{
 		for (column = 1; column <= size; column++)
 			_putchar('#');
