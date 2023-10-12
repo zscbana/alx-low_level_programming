f2e14d354f71dc79c7f0a46a702b75432853bd57
zscbana <zscbana@gmail.com>
2023-10-12 19:31:58 +0300

    auto commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,2 +1,19 @@
 #include "main.h"
-#include <
+#include <ctype.h>
+
+/**
+ * _isdigit - funcation used to detrmen digts
+ * @c: digt
+ *
+ * Return: if 1 (success) else 0 (fail)
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
