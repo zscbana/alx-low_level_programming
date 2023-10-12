4472708070f2d56f010ccc1cfba1fb46a679bf7d
zscbana <zscbana@gmail.com>
2023-10-12 21:41:09 +0300

    auto commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print a strgit line in terminal
+ * @n: number of -
+ */
+
+void print_line(int n)
+{
+	int j = 0;
+
+	for (j = 0; j < n; j++)
+	{
+		if (n == 0)
+			break;
+		_putchar('-');
+	}
+	_putchar('\n');
+}
