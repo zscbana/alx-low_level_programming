44ba9748a736226e514e149e3ee69f7bf25ec3c5
zscbana <zscbana@gmail.com>
2023-10-12 22:48:11 +0300

    auto commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,21 +4,22 @@
  * print_diagonal - draws a diagonal line on the terminal.
  * @n: number of diagonals
  */
-
 void print_diagonal(int n)
 {
 	int j, count;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (j = 0; j < n; j++)
 	{
-		if (n <= 0)
-			break;
-
 		for (count = 0; count < j; count++)
 		{
-			_putchar(' ');
+			_putchar('0');
 		}
-
 		_putchar('\\');
 		_putchar('\n');
 	}
