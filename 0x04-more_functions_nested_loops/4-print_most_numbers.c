cdea3d30d9171f86a052e4392780fd5b4541cf8b
zscbana <zscbana@gmail.com>
2023-10-12 20:24:40 +0300

    auto commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print from 0 to 9 ex 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+		}
+		_putchar(48 + i);
+	}
+}
