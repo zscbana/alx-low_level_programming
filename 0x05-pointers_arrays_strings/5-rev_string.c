0e5e1c50c631b8eafc1457477cde673f5d9b15e8
zscbana <zscbana@gmail.com>
2023-10-17 05:10:01 +0300

    auto commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reversesing a string.
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; l++)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
