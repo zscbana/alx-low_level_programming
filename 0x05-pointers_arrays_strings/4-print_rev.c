8fcb9d4e550461ceefaedf41f1c2e41f7ac9ea81
zscbana <zscbana@gmail.com>
2023-10-17 05:02:10 +0300

    auto commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
