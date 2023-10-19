281df3399b7d8cc9723b24a6412cc09a1656b4ca
zscbana <zscbana@gmail.com>
2023-10-19 20:24:58 +0300

    correction of task "3"

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,25 +2,31 @@
 
 /**
  * _strcmp - function that compares two strings.
- * @s1:pointer to the first string
- * @s2:pointer to the soucnd string
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
  *
- * Return: i= result
+ * Return: i = result
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (*s1)
+	while (*s1 && *s2)
 	{
 		if (*s1 != *s2)
 		{
-			i = ((int)*s1 - 48) - ((int)*s2 - 48);
+			i = (int)(*s1 - *s2);
 			break;
 		}
-		*s1++;
-		*s2++;
+		s1++;
+		s2++;
 	}
-	return (i);
+
+	if (i == 0)
+	{
+		i = (int)(*s1 - *s2);
+	}
+
+	return i;
 }
