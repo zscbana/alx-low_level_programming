11f3e9fad83732192a6fe1c0815de08ffe3ce51c
zscbana <zscbana@gmail.com>
2023-10-19 21:39:19 +0300

    2nd complex task"7"

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,29 @@
 #include "main.h"
 
 /**
- * 
- * 
-*/
+ * leet - a function that encodes a string into 1337.
+ * @s: pointer to array
+ *
+ * Return: modifed array
+ */
 
-char *leet(char *)
+char *leet(char *s)
 {
+	char *ss = s;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int val[] = {'4', '3', '0', '7', '1'};
+	unsigned int i;
 
+	while (*s)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*s == key[i] || *s == key[i] + 32)
+			{
+				*s = 48 + val[i];
+			}
+		}
+		s++;
+	}
+	return (ss);
 }
