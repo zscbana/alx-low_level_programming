d62be836e2452eb84088f97be514a4220c59e6fc
zscbana <zscbana@gmail.com>
2023-10-24 22:15:09 +0300

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,11 +3,20 @@
 /**
  * print_diagsums - a function that prints the sum ofthe two diagonals
  * of a square matrix of integers.
- * @a: pointer to array
- * @size: size of 
-*/
+ * @a: pointer to array of sum
+ * @size: size of dingonal
+ */
 
 void print_diagsums(int *a, int size)
 {
+	int i, sum1, sum2;
 
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + a[i];
+		sum2 += a[size - i - 1];
+		a = a + size;
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
