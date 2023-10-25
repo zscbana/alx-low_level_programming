a9ef74fbadbb2a038b4ad118659bfb94f45badd2
zscbana <zscbana@gmail.com>
2023-10-25 17:11:21 +0300

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s:pointer to array of char.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
