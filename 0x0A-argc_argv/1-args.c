1352accff618449dd91b14ebac42445231875efa
zscbana <zscbana@gmail.com>
2023-10-28 05:51:03 +0200

    fixing task 1 >3

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print number of argument passed
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 for success
+ * main - Print the number of arguments passed
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
+	(void)argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
