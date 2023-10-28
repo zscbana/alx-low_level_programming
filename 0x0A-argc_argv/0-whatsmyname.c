f53243914c3f11280099cd881a182670196db9dd
zscbana <zscbana@gmail.com>
2023-10-28 05:11:13 +0200

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print argument name follwed by new line
+ * @argc:int
+ * @argv:list
+ * Return: 0 success
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
