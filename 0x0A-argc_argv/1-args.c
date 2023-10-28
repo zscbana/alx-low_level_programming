4a75b72b101be68cf7886dc74d41aabe16d643e5
zscbana <zscbana@gmail.com>
2023-10-28 05:23:07 +0200

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print number of  argument passed
+ * @argc:int
+ * @argv:list
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	(void)argc;
+	printf("%d\n, argc -1");
+	return (0);
+}
