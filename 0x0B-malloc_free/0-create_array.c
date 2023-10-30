019e3426455d03c718c9af926e4cfc1eebef4a42
zscbana <zscbana@gmail.com>
2023-10-30 23:14:33 +0200

    task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - crate an array of char
+ * and initializes it with a specific char
+ * @size:size of array
+ * @c:specific char
+ *
+ * Return: array of char or 0 if null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a = malloc(sizeof(int) * size);
+
+	if (size == 0 || a == 0)
+		return (0);
+	while (size--)
+	{
+		a[size] = c;
+	}
+	return (a);
+}
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - funcation that print array of char
+ * @c: char
+ *
+ * Return: char or array of char
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
