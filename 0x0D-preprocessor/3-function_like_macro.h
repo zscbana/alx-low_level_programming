7f02a1afedb94d8885b195c8467740d342437a58
zscbana <zscbana@gmail.com>
2023-11-06 17:46:40 +0200

    fix task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) \
-	do { \
-		if ((x) < 0) \
-			(x) = -(x); \
-				} while (0)
+#define ABS(x) ((x < 0) ? -(x) : (x))
 
-#endif
+#endif 
