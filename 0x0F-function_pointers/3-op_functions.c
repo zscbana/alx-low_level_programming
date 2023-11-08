6996e19dc03853a772231da2faa21c6d9e67dd7e
zscbana <zscbana@gmail.com>
2023-11-08 19:35:26 +0200

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - funcation that add two numbers
+ * @a:first number
+ * @b:secound number
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - funcation that subtact two numbers
+ * @a:first number
+ * @b:secound number
+ * Return: the result of subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - funcation that multiplication two numbers
+ * @a:first number
+ * @b:secound number
+ * Return: the result of multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - funcation that deviation two numbers
+ * @a:first number
+ * @b:secound number
+ * Return: the result of deviation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - funcation that mod two numbers
+ * @a:first number
+ * @b:secound number
+ * Return: the result of mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
