ea1f163a5641cc40eb36ea98ba88a2990d85ddf1
zscbana <zscbana@gmail.com>
2023-11-09 16:44:27 +0200

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all -  a function that prints anything.
+ * @format:
+ * @...:
+ */
+
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	char *str;
+	unsigned int i = 0;
+	char c;
+	float f;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+
+		case 'c':
+			c = va_arg(args, int);
+			printf("%c", c);
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			f = va_arg(args, double);
+			printf("%f", f);
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+			break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+		 || format[i] == 's') && format[i + 1])
+			printf(", ");
+
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
