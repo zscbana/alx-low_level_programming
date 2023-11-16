7f30f0351a056b9710623676496c7a71bff09550
zscbana <zscbana@gmail.com>
2023-11-17 01:31:19 +0200

    task 4 correction

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,23 +1,23 @@
+
 #include "lists.h"
 
 /**
- * free_list - a function that frees a list_t list.
- * @head:pointer to head
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
  */
-
 void free_list(list_t *head)
 {
-	list_t *node, *next_node;
+    list_t *node, *next_node;
 
-	if (!head)
-		return;
+    if (!head)
+        return;
 
-	node = head;
-	while (node)
-	{
-		next_node = node->next;
-		free(node->str);
-		free(node);
-		node = next_node;
-	}
+    node = head;
+    while (node)
+    {
+        next_node = node->next;
+        free(node->str);
+        free(node);
+        node = next_node;
+    }
 }
