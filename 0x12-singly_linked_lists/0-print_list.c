ce923d49a43e9df2d4db5a964f074453398d2fee
zscbana <zscbana@gmail.com>
2023-11-17 01:36:36 +0200

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,38 +1,22 @@
 #include "lists.h"
 
 /**
- * _strlen - return the lenth of string
- * @s:string
- * Return: lenth of the string
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * print_list - prints a linked list
+ * print_list - print number of lists
  *
- * @h: pointer to the first node
- * Return: size of the list
+ * @h:pointer to header
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("[%d]%s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
+
+	return count;
 }
