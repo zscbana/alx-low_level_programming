91ca5e9ed98488e3f75262f08dd804cf6d7c7928
zscbana <zscbana@gmail.com>
2023-11-17 01:54:19 +0200

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,25 +1,56 @@
 #include "lists.h"
 
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to a pointer to the head of the list
+ * @str: string to be duplicated and added to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-    if (str == NULL)
-        return NULL;
+	list_t *new_node;
 
-    list_t *new_node = malloc(sizeof(list_t));
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    if (new_node == NULL)
-        return NULL;
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return NULL;
-    }
+	/* Duplicate the string using strdup */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+	/* Calculate the length of the string */
+	new_node->len = _strlen(new_node->str);
 
-    return new_node;
+	/* Set the next pointer of the new node to the current head */
+	new_node->next = *head;
+
+	/* Update the head to point to the new node */
+	*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: input string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
 }
