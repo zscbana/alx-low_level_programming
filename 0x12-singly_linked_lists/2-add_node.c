01a3c86ce9755adf01d29bce325f594e06d84589
zscbana <zscbana@gmail.com>
2023-11-17 01:42:59 +0200

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,24 +8,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
+    if (str == NULL)
+        return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
+    list_t *new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
-		return (NULL);
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
 
-	return (new_node);
+    return (new_node);
 }
