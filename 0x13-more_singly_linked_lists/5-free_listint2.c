51e0c267aad175784ac005cc43169774c7742b5f
zscbana <zscbana@gmail.com>
2023-11-20 22:29:01 +0200

    correction of task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (!head)
+	return;
+	
+
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
+	*head=NULL;
 }
