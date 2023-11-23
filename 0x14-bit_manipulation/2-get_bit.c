bd65e188629a8d8db5bc686f1c069d2174bbc50b
zscbana <zscbana@gmail.com>
2023-11-24 00:49:37 +0200

    fixing betty styel task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,13 +4,13 @@
  * get_bit - a function that returns the value of a bit at a given index.
  * @n:the number
  * @index: index of bit
- * Return: value of bit or -1 error 
+ * Return: value of bit or -1 error
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index>=sizeof(n)*8)
-	return (-1);
+	if (index >= sizeof(n) * 8)
+		return (-1);
 
-	return(n >> index & 1);
+	return (n >> index & 1);
 }
