9153e58d6c36e68d8a2932dea2393d9d84499580
zscbana <zscbana@gmail.com>
2023-11-28 00:21:43 +0200

    fixing task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,25 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to
- * the POSIX standard output.
- *
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed.
- * If the file cannot be opened or read, return 0.
- * If filename is NULL, return 0.
- * If write fails or does not write the expected amount of bytes, return 0.
- */
+* read_textfile - reads text from a file and prints it
+* @filename: name of file to read
+* @letters: number of bytes to read
+* Return: number bytes read / printed
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	bytes_read = read(fd, &buf[0], letters);
-	if (bytes_read == -1)
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes_written = write(STDOUT_FILENO, &buf[0], bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd);
-		return (0);
-	}
-
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
-	return (bytes_read);
+	return (bytes);
 }
