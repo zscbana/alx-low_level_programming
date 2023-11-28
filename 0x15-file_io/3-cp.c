efb5513ab735ed7bdc7f8176df828bc8224e45d0
zscbana <zscbana@gmail.com>
2023-11-28 23:44:32 +0200

    correction of task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-#define USAGE "Usage: cp file_from file_to"
+#define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
@@ -11,7 +11,7 @@
  * @ac: argument count
  * @av: argument vector
  *
- * Return:1 on success 0 on fail
+ * Return: 1 on success, 0 on failure
  */
 int main(int ac, char **av)
 {
@@ -21,22 +21,40 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
+
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
 	while ((b = read(fd_from, buf, READ_BUF_SIZE)) > 0)
-		if (write(fd_to, buf, b) != b)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	{
+		ssize_t bytes_written = 0;
+		while (bytes_written < b)
+		{
+			ssize_t result = write(fd_to, buf + bytes_written, b - bytes_written);
+			if (result == -1)
+			{
+				dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]);
+				close(fd_from);
+				close(fd_to);
+				exit(99);
+			}
+			bytes_written += result;
+		}
+	}
+
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	fd_from = close(fd_from);
-	fd_to = close(fd_to);
-	if (fd_from)
+
+	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
-	if (fd_to)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
-	return (0);
+
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_to), exit(100);
+
+	return 0;
 }
