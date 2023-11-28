785813f4b662f2bcbebc334b1eeb63c21b72e001
zscbana <zscbana@gmail.com>
2023-11-28 23:26:05 +0200

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+#define USAGE "Usage: cp file_from file_to"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+/**
+ * main - entry point
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return:1 on success 0 on fail
+ */
+int main(int ac, char **av)
+{
+	int fd_from = 0, fd_to = 0;
+	ssize_t b;
+	char buf[READ_BUF_SIZE];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	while ((b = read(fd_from, buf, READ_BUF_SIZE)) > 0)
+		if (write(fd_to, buf, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	if (b == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	fd_from = close(fd_from);
+	fd_to = close(fd_to);
+	if (fd_from)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
+	if (fd_to)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_to), exit(100);
+	return (0);
+}
